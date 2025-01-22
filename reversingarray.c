adca2857ab0d210b17a187ff28c08425c721b534
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2025-01-22 11:58:03 +0530

    Create reversingarray.c

---
+++ reversingarray.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int arr[] = {4,6,7,7,8987,5,6,89,8643};
+    //finding length of array
+    int size = sizeof(arr)/sizeof(arr[0]);
+    //reversing
+    for(int i=0, j=size-1;i<j;i++,j--)
+        arr[i] = arr[i]+arr[j] - (arr[j]=arr[i]);
+    //printing array elemts
+    for(int i=0;i<size;i++)
+        printf("%d ",arr[i]);
+}
