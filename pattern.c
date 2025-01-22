9b55a968653f5550e3f8821d600500f27d96a768
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2025-01-22 12:01:31 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main() {
+	//printf("%d %s\n", 1, "Aankit");
+
+	/*char str[] = "Nikhil";
+	printf("%-6.6s\n", str);
+	printf("%-6.5s\n", str);
+	printf("%-6.4s\n", str);
+	//Time complexity: T(n)
+	char str1[100];
+	int n;
+	scanf("%s%n", str, &n);
+	for (int i = n; i>=1;i--)
+	    printf("%-*.*s\n",n,i,str);
+
+	for (int i = n; i>=1;i--)
+	    printf("%*.*s%-*.*s\n",n,i,str,n,i,str);
+	for (int i = 1; i<=n;i++)
+	    printf("%*.*s%-*.*s\n",n,i,str,n,i,str);
+	for (int i = n; i>=1;i--)
+	    printf("%-*.*s%*.*s\n",n,i,str,n,i,str);
+	for (int i = 1; i<=n;i++)
+	    printf("%-*.*s%*.*s\n",n,i,str,n,i,str);*/
+    //3rd
+    /*char str[100];
+    int n;
+    scanf("%s%n",str,&n);
+    for(int i=1;i<n;i++)
+        printf("%*c%*c\n",i,str[i-1],n*2-(i*2), str[i-1]);
+    printf("%*c\n",n,str[n-1]);
+    for(int i=n-1;i>=1;i--)
+        printf("%*c%*c\n",i,str[i-1],n*2-(i*2), str[i-1]);*/
+    return 0;
+}
