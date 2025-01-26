000323fe04690658fe37b9147a258402e33dbb99
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2025-01-26 22:28:12 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        float f = 1.0f;
+        float g = 4.0f;
+        float result = f * g;
+        System.out.println(result);
+    }
+    
+
+    
+    
+}
