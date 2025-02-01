ad3cc628db25dd6ea64c456f7fe0829526e5c2a4
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2025-02-01 21:33:57 +0530

    Add files via upload

---
+++ Progtodisplay1to10.java
@@ -0,0 +1,9 @@
+public class Progtodisplay1to10 {
+    public static void main(String[] args) {
+        int i = 1, n = 10;
+        while(i<=n){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
